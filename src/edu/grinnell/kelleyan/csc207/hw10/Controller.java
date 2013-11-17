b0921eec86f4572505c8bf08f1525c4787e801d8
southpaw27 <awkelley27@gmail.com>
2013-11-17 15:49:23 -0600

    Controller methods started, Evaluator methods pretty much finished

--- src/edu/grinnell/kelleyan/csc207/hw10/Controller.java
+++ src/edu/grinnell/kelleyan/csc207/hw10/Controller.java
@@ -1,13 +1,45 @@
 package edu.grinnell.kelleyan.csc207.hw10;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+import edu.grinnell.glimmer.ushahidi.UshahidiWebClient;
+
 public class Controller {
 
-    /**
-     * @param args
+    /*
+     * Fields
      */
-    public static void main(String[] args) {
-	// TODO Auto-generated method stub
+    PriorityQueue current;
+    Evaluator eval;
 
+    /*
+     * Constructors
+     */
+    public Controller() {
+	eval = null;
+	current = null;
     }
 
+    public void makeServer(String str) throws Exception {
+	UshahidiWebClient web = new UshahidiWebClient(str);
+	eval = new Evaluator(web);
+    }
+
+    public void makeQueue(String str, int i) {
+	// Make the comparator
+	Comparator c;
+	switch (str) {
+	case "latitude":
+	    break;
+	case "longitude":
+	    break;
+	case "id":
+	    break;
+	case "date":
+	    break;
+	} // switch
+	  // get the queue given that comparator
+	this.current = this.eval.formQueue(c);
+    }
 }
